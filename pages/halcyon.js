b9231e9295013c225b781fee58417e4dec9f8629
richardphamdq <70403005+richardphamdq@users.noreply.github.com>
2022-04-28 19:40:58 -0500

    Richard, Update halcyon page info, check numbers and changed base 11 section

--- pages/halcyon.js
+++ pages/halcyon.js
@@ -30,24 +30,23 @@ export default function Halcyon () {
                         <li><p>thrust <br/>3,500 lbf</p></li>
                         <li><p>propellants <br/>RP-1 LOx</p></li>
                         <li><p>apogee <br/>100+ km</p></li>
-                        <li><p>payload <br/>11 lbs</p></li>
+                        <li><p>payload <br/>13.2 lbs</p></li>
                         <li><p>max speed <br/>mach 3.5</p></li>
                     </ul>
                 </div>
                 <img className={styles.halcyon} src={'./images/Halcyon.png'} />
             </section>
-            <section className={styles.compete}>
+            < section className={styles.compete}>
                 <div className={styles.container}>
-                    <h1>Built to Compete</h1>
-                    <p>Our Halcyon rocket is going head-to-head with
-    other student rocket groups across America.</p>
+                    <h1>Join Us</h1>
+                    <p>Halcyon is ushering in a new age of ambitious rocketeers and innovators who are shaping the future of space. Find out how to contribute to our dream of reaching the Karman Line.</p>
                 <div className={styles.flex}>
-                    <img src={'./logos/baseeleven.svg'} />
-                    <a className={utilStyles.textbutton} href="https://base11spacechallenge.org/" target="_blank">learn more</a>
+                    <Link href={'/join'}><a className={styles.apply}>How to Apply</a></Link>
+                    <Link href={'/donate'}><a className={styles.donate}>Donate</a></Link>
                 </div>
                 </div>
                 <img className={styles.vacuum} src={'./images/vacuum.png'} />
-            </section>
+            </section >
             {/* <section className={styles.havoc}>
                 <div>
                     <h1>Built to Compete</h1>
@@ -55,7 +54,7 @@ export default function Halcyon () {
     other student rocket groups across America.</p>
                 </div>
                 <img className={styles.havocfire} src={'./images/havoc.png'} />
-            </section> */}
+        </section> */}
         </Layout>
     )
 }
