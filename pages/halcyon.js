d3a4c2e5f7dac343a4b1a1d5c0fa905c5791bacf
bennto <uxttenneb@gmail.com>
2023-05-29 16:38:15 -0500

    Halcyon page parts update (incomplete)

--- pages/halcyon.js
+++ pages/halcyon.js
@@ -59,13 +59,23 @@ export default function Halcyon() {
         <div className={styles.section3}>
           <div className={styles.stickyContainer}>
             <div className={styles.details}>
-              <h2>Engine</h2>
-              <p>
+              <h3 className={styles.partName}>Engine</h3>
+              <p className={styles.partBody}>
                 Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
                 Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
                 Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
                 Water Gun Staryu Dugtrio.
               </p>
+              <h4 className={styles.partSub}>Details</h4>
+              <hr className={styles.partDivide} />
+              <ol className={styles.partBullets}>
+                <li>Kerolox engine</li>
+                <li>Regeneratively cooled</li>
+                <li>Additively manufactured / 3D printed</li>
+                <li>Nominally 3500 lb of thrust</li>
+                <li>~ 60s burn duration</li>
+                <li>Pressure fed engine cycle</li>
+              </ol>
             </div>
             <div className={styles.imgContainer}>
               <img
@@ -82,13 +92,27 @@ export default function Halcyon() {
         <div className={styles.section3}>
           <div className={styles.stickyContainer}>
             <div className={styles.details}>
-              <h2>Lorem</h2>
-              <p>
-                Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
-                Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
-                Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
-                Water Gun Staryu Dugtrio.
+              <h3 className={styles.partName}>TVC</h3>
+              <p className={styles.partBody}>
+                Thrust Vector Control (TVC), is a system in Halcyon allows us to
+                adjust the rocket's trajectory during its upward ascent. With
+                this system, we can account for any changes in the rockets
+                trajectory with speed and accuracy, ensuring our rocket stays on
+                its path to the Karman Line. TVC is comprised of a system of
+                rings that are attached to our engine to rotate it around. Each
+                ring is connected to a linear actuator that pushes and pulls on
+                the ring.
               </p>
+              <h4 className={styles.partSub}>Details</h4>
+              <hr className={styles.partDivide} />
+              {/* <ol className={styles.partBullets}>
+                <li>Kerolox engine</li>
+                <li>Regeneratively cooled</li>
+                <li>Additively manufactured / 3D printed</li>
+                <li>Nominally 3500 lb of thrust</li>
+                <li>~ 60s burn duration</li>
+                <li>Pressure fed engine cycle</li>
+              </ol> */}
             </div>
             <div className={styles.imgContainer}>
               <img
@@ -99,61 +123,69 @@ export default function Halcyon() {
           </div>
           <div className="h-screen" />
           <div className="h-screen" />
-        </div>
-        <div className="h-screen" />
-        <div className={styles.section3}>
-          <div className={styles.stickyContainer}>
-            <div className={styles.details}>
-              <h2>Lorem</h2>
-              <p>
-                Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
-                Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
-                Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
-                Water Gun Staryu Dugtrio.
-              </p>
-            </div>
-            <div className={styles.imgContainer}>
-              <img
-                className={styles.image}
-                src={"../images/Halcyon/HalcyonLaunchSunsetNoSun.png"}
-              />
-            </div>
-          </div>
-          <div className="h-screen" />
-        </div>
-        <div className="h-screen" />
-        <div className={styles.section3}>
-          <div className={styles.stickyContainer}>
-            <div className={styles.details}>
-              <h2>Lorem</h2>
-              <p>
-                Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
-                Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
-                Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
-                Water Gun Staryu Dugtrio.
-              </p>
-            </div>
-            <div className={styles.imgContainer}>
-              <img
-                className={styles.image}
-                src={"../images/Burst Stand Action Torque 1-21.JPG"}
-              />
-            </div>
-          </div>
         </div>
         <div className="h-screen" />
         {/* <div className={styles.section3}>
-          <div className={styles.backgroundFrame}>
-            <img
-              className={styles.background}
-              src={"../images/BenJ Test Stand Walkie 1-31.JPG"}
-            ></img>
+          <div className={styles.stickyContainer}>
+            <div className={styles.details}>
+              <h3 className={styles.partName}>Engine</h3>
+              <p className={styles.partBody}>
+                Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
+                Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
+                Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
+                Water Gun Staryu Dugtrio.
+              </p>
+              <h4 className={styles.partSub}>Details</h4>
+              <hr className={styles.partDivide} />
+              <ol className={styles.partBullets}>
+                <li>Kerolox engine</li>
+                <li>Regeneratively cooled</li>
+                <li>Additively manufactured / 3D printed</li>
+                <li>Nominally 3500 lb of thrust</li>
+                <li>~ 60s burn duration</li>
+                <li>Pressure fed engine cycle</li>
+              </ol>
+            </div>
+            <div className={styles.imgContainer}>
+              <img
+                className={styles.image}
+                src={"../images/Halcyon/HalcyonLaunchSunsetNoSun.png"}
+              />
+            </div>
           </div>
-          <div className={styles.content}>
-            <h1>Halcyon</h1>
-            <h6>TREL's very own Big Rocket</h6>
+          <div className="h-screen" />
+        </div>
+        <div className="h-screen" />
+        <div className={styles.section3}>
+          <div className={styles.stickyContainer}>
+            <div className={styles.details}>
+              <h3 className={styles.partName}>Engine</h3>
+              <p className={styles.partBody}>
+                Pokem ipsum dolor sit amet Shiftry Combee Aipom Gastrodon
+                Professor Oak Manectric. Kanto Cerulean City Haxorus Electric
+                Abomasnow Porygon2 Cubchoo. Leaf Green Donphan Baltoy Unown
+                Water Gun Staryu Dugtrio.
+              </p>
+              <h4 className={styles.partSub}>Details</h4>
+              <hr className={styles.partDivide} />
+              <ol className={styles.partBullets}>
+                <li>Kerolox engine</li>
+                <li>Regeneratively cooled</li>
+                <li>Additively manufactured / 3D printed</li>
+                <li>Nominally 3500 lb of thrust</li>
+                <li>~ 60s burn duration</li>
+                <li>Pressure fed engine cycle</li>
+              </ol>
+            </div>
+            <div className={styles.imgContainer}>
+              <img
+                className={styles.image}
+                src={"../images/Burst Stand Action Torque 1-21.JPG"}
+              />
+            </div>
           </div>
-        </div> */}
+        </div>
+        <div className="h-screen" /> */}
       </div>
     </Layout>
   );
